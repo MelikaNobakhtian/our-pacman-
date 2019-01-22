9c9393512fb813622e7af16051b9e878a8baa3ce
melikano2000 <45814324+melikano2000@users.noreply.github.com>
2019-01-22 20:00:54 +0330

    Add files via upload

--- Pac-Man.cpp
+++ Pac-Man.cpp
@@ -17,7 +17,10 @@ SDL_Surface* Surface=NULL;
 SDL_Window* window=NULL;
 SDL_Renderer* rend=NULL;
 bool quit=false;
+int count=0;
+int score=0;
 SDL_Event event;
+class pacman;
 int i;
 int Blocks[55][4]={{20,68,620,78},{20,68,30,282},{20,272,138,282},{128,272,138,356},{20,346,138,356},
 {20,400,138,410},{128,400,138,484},{20,474,138,484},{20,484,30,732},{20,722,620,732},
@@ -29,7 +32,9 @@ int Blocks[55][4]={{20,68,620,78},{20,68,30,282},{20,272,138,282},{128,272,138,3
 {246,410,394,420},{182,400,202,484},{438,400,458,484},{246,464,394,484},{310,464,330,548},
 {74,528,138,548},{118,528,138,612},{182,528,266,548},{374,528,458,548},{502,528,566,548},
 {502,528,522,612},{20,592,74,612},{566,592,620,612},{74,656,256,676},{182,592,202,676},
-{236,592,384,612},{300,592,320,676},{374,656,566,676},{438,592,458,676},{298,338,342,344}};
+{246,592,394,612},{310,592,330,676},{374,656,566,676},{438,592,458,676},{298,338,342,344}};
+
+int dots[300][2];
 
 class ghost
 {
@@ -40,12 +45,13 @@ public:
       int y2;
       float vx;
       float vy;
+      bool turnedblue=false;
       SDL_Rect png[8];
       SDL_Texture* ghost;
       SDL_Rect blue[4];
       SDL_Texture* blueghost;
       SDL_Rect eyes[4];
-      SDL_Rect ghostpos={x1,y1,x2-x1,y2-y1};
+      SDL_Rect ghostpos={x1,y1,44,44};
       SDL_Texture* ghosteyes;
       int direction;
       void initghost ( std::string path ){
@@ -86,22 +92,117 @@ public:
 		SDL_FreeSurface( ghostsurf );
 	  }
 	  
-      void show(int x1, int y1)
+      void show(int x, int y/*,int v,int v2*/)
 	  {
+		x1=x;
+		y1=y;
+		//vx=v;
+		//vy=v2;
+	  	x1+=vx;
+	  	y1-=vy;
+	  	ghostCollision();
 	  	if(direction%2){
 	  		direction--;
 	  	}else{
 	  		direction++;
 	  	}
-	  	ghostpos.x=x1; ghostpos.y=y1; ghostpos.w=44; ghostpos.h=44;
+	  	ghostpos.x=x1; ghostpos.y=y1;
 	  	SDL_RenderCopy(rend,ghost,&png[direction],&ghostpos);
       }
 
      
 
-      void changepng(){
+    void ghostCollision(){
+	bool up=false,down=false,right=false,left=false;
+	for(i=0; i<54; i++){
+		if(vx!=0){
+			if(x1+44==Blocks[i][0] && ((y1+1>=Blocks[i][1] && y1+1<=Blocks[i][3]) || (y1+43>=Blocks[i][1] && y1+43<=Blocks[i][3]) || (Blocks[i][1]>y1 && Blocks[i][1]<y1+44)) ){
+				right=true;
+			}
+			else if(x1==Blocks[i][2] && ((y1+1>=Blocks[i][1] && y1+1<=Blocks[i][3]) || (y1+43>=Blocks[i][1] && y1+43<=Blocks[i][3]) || (Blocks[i][1]>y1 && Blocks[i][1]<y1+44)) ){
+				left=true;
+			}
+		}
+		else{
+			if(y1+44==Blocks[i][1] && ((x1+1>=Blocks[i][0] && x1+1<=Blocks[i][2]) || (x1+43>=Blocks[i][0] && x1+43<=Blocks[i][2]) || (Blocks[i][0]>x1 && Blocks[i][0]<x1+44)) ){
+				down=true;
+			}
+			else if(y1==Blocks[i][3] && ((x1+1>=Blocks[i][0] && x1+1<=Blocks[i][2]) || (x1+43>=Blocks[i][0] && x1+43<=Blocks[i][2]) || (Blocks[i][0]>x1 && Blocks[i][0]<x1+44)) ){
+				up=true;
+			}
+		}
+	}
+	if(vx!=0){
+		if((right && vx>0) || (left && vx<0)){
+			vx=0;
+			if(rand()%2){
+				if(down) vy=2;
+				else vy=-2;
+				changepng();
+			}
+			else{
+				if(up) vy=-2;
+				else vy=2;
+				changepng();
+			}
+		}
+		else if((!right && vx>0) || (!left && vx<0)){
+			switch(rand()%3){
+				case 0: if(up){
+					vx=0;
+					vy=-2;
+					changepng();
+					break;
+				}
+				case 1: if(down){
+					vx=0;
+					vy=2;
+					changepng();
+					break;
+				}
+			}
+		}
+	}else{
+		if((down && vy<0) || (up && vy>0)){
+			vy=0;
+			if(rand()%2){
+				if(left) vx=2;
+				else vx=-2;
+				changepng();
+			}
+			else{
+				if(right) vx=-2;
+				else vx=2;
+				changepng();
+			}
+		}
+		else if((!down && vy<0) || (!up && vy>0)){
+			switch(rand()%3){
+				case 0: if(left){
+					vy=0;
+					vx=-2;
+					changepng();
+					break;
+				}
+				case 1: if(right){
+					vy=0;
+					vx=2;
+					changepng();
+					break;
+				}
+			}
+		}
+	}
+} 
 
+      void changepng(){
+		if(vx>0) direction=6;
+		else if(vx<0) direction=4;
+		else if(vy<0) direction=2;
+		else direction=0;
       }
+
+      void ghostpacmanCollision();
 };
 ghost Inky;
 ghost Pinky;
@@ -116,13 +217,14 @@ class pacman
     int x2;
     int y1;
     int y2;
-    float vx;
-    float vy;
+    float vx=0;
+    float vy=0;
     SDL_Rect pac[9];
     SDL_Texture* pacman;
     SDL_Rect dead[11];
     SDL_Texture* deadpac;
-    SDL_Rect Pacmanpos={x1,y1,x2-x1,y2-y1};
+    SDL_Rect Pacmanpos={x1,y1,44,44};
+    int pacdirection;
 
 void initpacman( std::string path ){
 	 SDL_Surface* pacsurf= IMG_Load( path.c_str() );
@@ -133,7 +235,7 @@ void initpacman( std::string path ){
 			pac[i].w=44;
 			pac[i].h=44;
 		}
-	pac[9].x=88; pac[9].y=0; pac[9].w=44; pac[9].h=44;
+	pac[8].x=88; pac[8].y=0; pac[8].w=44; pac[8].h=44;
 	SDL_FreeSurface( pacsurf );
 }
 
@@ -149,7 +251,24 @@ void initdead( std::string path ){
 	SDL_FreeSurface( pacsurf );
 }
 
-void show();
+bool pacshow(int x, int y){
+	x1=x;
+	y1=y;
+	PacmanWallCollision(vx,vy);
+	x1+=vx;
+	y1-=vy;
+	if(vx || vy){
+		if(pacdirection%2){
+	  		pacdirection--;
+	  	}else{
+	  		pacdirection++;
+	  	}
+	}else if(pacdirection%2){
+		pacdirection--;
+	}
+ 	Pacmanpos.x=x1; Pacmanpos.y=y1;
+	SDL_RenderCopy(rend,pacman,&pac[pacdirection],&Pacmanpos);
+}
 
  void move(int x1,int x2,int y1,int y2,float vx,float vy){
       //if(){
@@ -162,9 +281,73 @@ void show();
       }*/
       }
 
+ bool PacmanWallCollision(int v1, int v2){
+    bool up=false,down=false,right=false,left=false;
+	for(i=0; i<54; i++){
+		if(v1!=0){
+			if(x1+44==Blocks[i][0] && ((y1+1>=Blocks[i][1] && y1+1<=Blocks[i][3]) || (y1+43>=Blocks[i][1] && y1+43<=Blocks[i][3]) || (Blocks[i][1]>y1 && Blocks[i][1]<y1+44)) ){
+				right=true;
+			}
+			else if(x1==Blocks[i][2] && ((y1+1>=Blocks[i][1] && y1+1<=Blocks[i][3]) || (y1+43>=Blocks[i][1] && y1+43<=Blocks[i][3]) || (Blocks[i][1]>y1 && Blocks[i][1]<y1+44)) ){
+				left=true;
+			}
+		}
+		else{
+			if(y1+44==Blocks[i][1] && ((x1+1>=Blocks[i][0] && x1+1<=Blocks[i][2]) || (x1+43>=Blocks[i][0] && x1+43<=Blocks[i][2]) || (Blocks[i][0]>x1 && Blocks[i][0]<x1+44)) ){
+				down=true;
+			}
+			else if(y1==Blocks[i][3] && ((x1+1>=Blocks[i][0] && x1+1<=Blocks[i][2]) || (x1+43>=Blocks[i][0] && x1+43<=Blocks[i][2]) || (Blocks[i][0]>x1 && Blocks[i][0]<x1+44)) ){
+				up=true;
+			}
+		}
+	} 
+	if(v1==vx && v2==vy){
+		if(vx>0 && right) vx=0;
+	else if(vx<0 && left) vx=0;
+	else if(vy>0 && up) vy=0;
+	else if(vy<0 && down) vy=0;
+	return true;
+	}
+	else {
+		if((v1>0 && right) || (v1<0 && left) || (v2>0 && up) || (v2<0 && down)) return false;
+		else return true;
+	}
+}
+
+	void showdead(){
+		for(i=0;i<12;i++){
+		SDL_Delay(50);
+		SDL_SetRenderDrawColor( rend, 0, 0, 0, 0xFF );
+		SDL_RenderClear( rend );
+		for(int i=0;i<54;i++){
+					boxRGBA(rend,Blocks[i][0],Blocks[i][1],Blocks[i][2],Blocks[i][3],0,0,255,255);
+				}
+		boxRGBA(rend,Blocks[54][0],Blocks[54][1],Blocks[54][2],Blocks[54][3],255,255,255,255);
+		Pinky.show(Pinky.x1,Pinky.y1);
+		Inky.show(Inky.x1,Inky.y1);
+		Blinky.show(Blinky.x1,Blinky.y1);
+		Clyde.show(Clyde.x1,Clyde.y1);
+		SDL_RenderCopy(rend,deadpac,&dead[i],&Pacmanpos);
+		SDL_RenderPresent( rend );
+		}
+	}
+	
+
 
 };
 pacman Player;
+
+void ghost::ghostpacmanCollision(){
+      	if(turnedblue){
+      		if( SDL_HasIntersection( &ghostpos , &Player.Pacmanpos )){
+		count++;
+		score+=count*200;
+		//ghost.showeyes();
+		}
+	} else if( SDL_HasIntersection( &ghostpos,&Player.Pacmanpos) ){
+		Player.showdead();
+		}
+}
 
 class fruit
 {
@@ -173,24 +356,46 @@ class fruit
 	int x2;
 	int y1;
 	int y2;
-	SDL_Rect fruit;
 	SDL_Texture* food;
-	SDL_Rect fruitpos={x1,y1,x2-x1,y2-y1};
+	SDL_Rect fruitpos={x1,y1,44,44};
+	bool appeared=false;
+	int timetoappear= 150;
+	int timetogo=345;
 
 	void initfruit( std::string path ){
 		SDL_Surface* fruitsurf= IMG_Load( path.c_str() );
       	food=SDL_CreateTextureFromSurface( rend, fruitsurf);
-		fruit.x=0;
-		fruit.y=0;
-		fruit.w=44;
-		fruit.h=44;
       SDL_FreeSurface( fruitsurf);
 	}
 
-	
+	void showfruit(int x, int y){
+		x1=x;
+		y1=y;
+		fruitpos.x=x1; fruitpos.y=y1;
+		SDL_RenderCopy(rend,food,NULL,&fruitpos);
+	}
+
+	void fruitCollision(){
+		if( SDL_HasIntersection( &fruitpos , &Player.Pacmanpos )){
+			appeared=false;
+			switch(rand()%5){
+				case 0: score+=100;
+				break;
+				case 1: score+=300;
+				break;
+				case 2: score+=500;
+				break;
+				case 3: score+=700;
+				break;
+				default: score+=1000;
+			}
+		}
+	}
 	
 };
 fruit Foody;
+
+
 
 void init(){
 	
@@ -215,14 +420,6 @@ void init(){
 	 
 }
 
-//SDL_Texture* loadTexture( std::string path )
-//{
-//    SDL_Texture* newTexture = NULL;
-//    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-//    SDL_Texture* newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-//    SDL_FreeSurface( loadedSurface );
-//    return newTexture;
-//}
 
 void close()
 {
@@ -249,6 +446,179 @@ void close()
 
 int main( int argc, char* args[] )
 {
+
+for(i=1;i<27;i+=2){
+	if(i==13)continue;
+	dots[i-1][0]=30+((i+1)*11+(i-1)/2*21)-3;
+	dots[i-1][1]=78+19;
+}
+for(i=27;i<53;i+=2){
+	dots[i-1][0]=30+((i-26+1)*11+(i-27)/2*21)-3;
+	dots[i-1][1]=78+105;
+}
+for(i=53;i<79;i+=2){
+	if(i==59 || i==65 || i==71) continue;
+	dots[i-1][0]=30+((i-51)*11+(i-53)/2*21)-3;
+	dots[i-1][1]=78+170;
+}
+for(i=79; i<105; i+=2){
+	if(i==91)continue;
+	dots[i-1][0]=30+((i-77)*11+(i-79)/2*21)-3;
+	dots[i-1][1]=503;
+}
+for(i=105;i<131;i+=2){
+	if(i==111 || i==117 || i==123) continue;
+	dots[i-1][0]=30+((i-103)*11+(i-105)/2*21)-3;
+	dots[i-1][1]=631;
+}
+for(i=131;i<157;i+=2){
+	dots[i-1][0]=30+((i-130+1)*11+(i-131)/2*21)-3;
+	dots[i-1][1]=697;
+}
+for(i=157;i<183;i+=2){
+	if(i==161 || i==179) continue;
+	dots[i-1][0]=30+((i-156+1)*11+(i-157)/2*21)-3;
+	dots[i-1][1]=567;
+}
+for(i=2;i<28;i+=2){
+	if(i==14)continue;
+	dots[i-1][0]=30+((i)*11+(i)/2*21)-2;
+	dots[i-1][1]=78+20;
+}
+for(i=28;i<54;i+=2){
+	dots[i-1][0]=30+((i-26)*11+(i-26)/2*21)-2;
+	dots[i-1][1]=78+106;
+}
+for(i=54;i<80;i+=2){
+	if(i==60 || i==66 || i==72) continue;
+	dots[i-1][0]=30+((i-52)*11+(i-52)/2*21)-2;
+	dots[i-1][1]=78+171;
+}
+for(i=80; i<106; i+=2){
+	if(i==92)continue;
+	dots[i-1][0]=30+((i-78)*11+(i-78)/2*21)-2;
+	dots[i-1][1]=504;
+}
+for(i=106;i<132;i+=2){
+	if(i==112 || i==118 || i==124) continue;
+	dots[i-1][0]=30+((i-104)*11+(i-104)/2*21)-2;
+	dots[i-1][1]=632;
+}
+for(i=132;i<158;i+=2){
+	dots[i-1][0]=30+((i-130)*11+(i-130)/2*21)-2;
+	dots[i-1][1]=698;
+}
+for(i=158;i<184;i+=2){
+	if(i==160 || i==178) continue;
+	dots[i-1][0]=30+((i-156)*11+(i-156)/2*21)-2;
+	dots[i-1][1]=568;
+}
+dots[12][0]=49; dots[12][1]=140;
+dots[58][0]=350; dots[58][1]=140;
+dots[64][0]=477; dots[64][1]=140;
+
+
+dots[70][0]=157; dots[70][1]=117;
+dots[90][0]=285; dots[90][1]=117;
+dots[110][0]=585; dots[110][1]=117;
+
+
+dots[13][0]=158; dots[13][1]=140;
+dots[59][0]=286; dots[59][1]=140;
+dots[65][0]=586; dots[65][1]=140;
+
+
+dots[71][0]=50; dots[71][1]=118;
+dots[91][0]=351; dots[91][1]=118;
+dots[111][0]=478; dots[111][1]=118;
+
+
+dots[182][0]=285;dots[182][1]=161;
+dots[184][0]=585;dots[184][1]=161;
+dots[186][0]=157;dots[186][1]=161;
+
+
+dots[183][0]=50;dots[183][1]=162;
+dots[185][0]=351;dots[185][1]=162;
+dots[187][0]=478;dots[187][1]=162;
+
+
+dots[189][0]=50;dots[189][1]=225;
+dots[191][0]=50;dots[191][1]=205;
+
+dots[193][0]=478;dots[193][1]=225;
+dots[195][0]=478;dots[195][1]=205;
+
+dots[197][0]=586;dots[197][1]=225;
+dots[199][0]=586;dots[199][1]=205;
+
+dots[201][0]=158;dots[201][1]=225;
+dots[203][0]=158;dots[203][1]=205;
+
+dots[205][0]=222;dots[205][1]=225;
+dots[207][0]=222;dots[207][1]=205;
+
+dots[209][0]=415;dots[209][1]=225;
+dots[211][0]=415;dots[211][1]=205;
+
+for(i=189;i<207;i+=2){
+	if(i==203)continue;
+	dots[i-1][1]=78+170+((i-188+1)*11+(i-189)/2*21)-3;
+	dots[i-1][0]=477;
+}
+for(i=214;i<230;i+=2){
+	if(i==224 )continue;
+	dots[i-1][1]=78+170+((i-212)*11+(i-212)/2*21)-2;
+	dots[i-1][0]=478;
+}
+
+for(i=207;i<225;i+=2){
+	if(i==221)continue;
+	dots[i-1][1]=78+170+((i-206+1)*11+(i-207)/2*21)-3;
+	dots[i-1][0]=157;
+}
+for(i=230;i<246;i+=2){
+	if(i==240 )continue;
+	dots[i-1][1]=78+170+((i-228)*11+(i-228)/2*21)-2;
+	dots[i-1][0]=158;
+}
+dots[245][0]=50;dots[245][1]=524;
+dots[247][0]=50;dots[247][1]=547;
+dots[249][0]=587;dots[249][1]=524;
+dots[251][0]=587;dots[251][1]=547;
+
+dots[253][0]=286;dots[253][1]=524;
+dots[255][0]=286;dots[255][1]=547;
+dots[257][0]=351;dots[257][1]=524;
+dots[259][0]=351;dots[259][1]=547;
+
+dots[261][0]=50;dots[261][1]=652;
+dots[263][0]=50;dots[263][1]=675;
+dots[265][0]=587;dots[265][1]=652;
+dots[267][0]=587;dots[267][1]=675;
+
+dots[269][0]=286;dots[269][1]=652;
+dots[271][0]=286;dots[271][1]=675;
+dots[273][0]=351;dots[273][1]=652;
+dots[275][0]=351;dots[275][1]=675;
+
+dots[269][0]=93;dots[269][1]=588;
+dots[271][0]=93;dots[271][1]=611;
+
+dots[273][0]=221;dots[273][1]=588;
+dots[275][0]=221;dots[275][1]=611;
+
+dots[277][0]=415;dots[277][1]=588;
+dots[279][0]=415;dots[279][1]=611;
+
+dots[281][0]=544;dots[281][1]=588;
+dots[283][0]=544;dots[283][1]=611;
+
+dots[285][0]=286;dots[285][1]=652;
+dots[287][0]=286;dots[287][1]=675;
+
+dots[289][0]=351;dots[289][1]=652;
+dots[291][0]=351;dots[291][1]=675;
 	srand(time(0));
 	init();
 	Blinky.initghost("Blinky.png");
@@ -263,13 +633,19 @@ int main( int argc, char* args[] )
 	Inky.initeyes("A pair of Eyes.png");
 	Pinky.initeyes("A pair of Eyes.png");
 	Clyde.initeyes("A pair of Eyes.png");
-	Player.initpacman("Pacman");
+	Player.initpacman("Pacman.png");
+	Foody.initfruit("Cherry.png");
 	//Player.initdead("");
-	Foody.initfruit("Cherry");
 	Pinky.direction=1;
 	Blinky.direction=3;
 	Inky.direction=5;
 	Clyde.direction=6;
+	Player.pacdirection=2;
+	Blinky.x1=298; Blinky.y1=292; Blinky.vx=2;Blinky.vy=0;
+	Pinky.x1=254;Pinky.y1=356;Pinky.vx=0; Pinky.vy=-2;
+	Inky.x1=298;Inky.y1=356;Inky.vx=-2; Inky.vy=0;
+	Clyde.x1=342;Clyde.y1=356;Clyde.vx=0; Clyde.vy=2;
+	Player.x1=298; Player.y1=420;
 	while(!quit){
 		while(SDL_PollEvent(&event)!=0){
 			if( event.type == SDL_QUIT )
@@ -277,18 +653,82 @@ int main( int argc, char* args[] )
 				quit = true;
 				return 0;
 			}
+			else if(event.type==SDL_KEYDOWN){
+                switch(event.key.keysym.sym){
+                	case SDLK_LEFT:
+                	if(Player.PacmanWallCollision(-2,0)){
+                		Player.vy=0;
+                		Player.vx=-2;
+                		if(Player.pacdirection!=0 && Player.pacdirection!=1)
+                			Player.pacdirection=0;
+                	}
+                	break;
+                	case SDLK_RIGHT:
+                	if(Player.PacmanWallCollision(2,0)){
+                		Player.vy=0;
+                		Player.vx=2;
+                		if(Player.pacdirection!=2 && Player.pacdirection!=3)
+                			Player.pacdirection=2;
+                	}
+                	break;
+                	case SDLK_UP:
+                	if(Player.PacmanWallCollision(0,2)){
+                		Player.vy=2;
+                		Player.vx=0;
+                		if(Player.pacdirection!=4 && Player.pacdirection!=5)
+                			Player.pacdirection=4;
+                	}
+                	break;
+                	case SDLK_DOWN:
+                	if(Player.PacmanWallCollision(0,-2)){
+                		Player.vy=-2;
+                		Player.vx=0;
+                		if(Player.pacdirection!=6 && Player.pacdirection!=7)
+                			Player.pacdirection=6;
+                	}
+                	break;
+                }
+            }
 		}
-		SDL_Delay(200);
+		SDL_Delay(50);
+		if(Foody.appeared){
+			if(rand()%500==Foody.timetogo){
+				Foody.appeared=false;
+			}
+		}else{
+			if(rand()%500==Foody.timetoappear){
+				Foody.appeared=true;
+			}
+		}
 		SDL_SetRenderDrawColor( rend, 0, 0, 0, 0xFF );
 		SDL_RenderClear( rend );
 		for(int i=0;i<54;i++){
 					boxRGBA(rend,Blocks[i][0],Blocks[i][1],Blocks[i][2],Blocks[i][3],0,0,255,255);
 				}
 		boxRGBA(rend,Blocks[54][0],Blocks[54][1],Blocks[54][2],Blocks[54][3],255,255,255,255);
-		Pinky.show(254,356);
-		Inky.show(298,356);
-		Blinky.show(298,292);
-		Clyde.show(342,356);
+		if(Foody.appeared){
+			Foody.showfruit(298,420);
+			Foody.fruitCollision();
+		} 
+		for(i=0;i<224;i+=2 ){
+			if(i==202 || i==220) continue;
+				boxRGBA(rend,dots[i][0],dots[i][1],dots[i][0]+5,dots[i][1]+5,249, 218, 197,255);
+			}
+
+		for(i=1;i<293;i+=2 ){
+			if(i==223 || i==239) continue;
+				boxRGBA(rend,dots[i][0],dots[i][1],dots[i][0]+4,dots[i][1]+4,249, 218, 197,255);
+			}
+		Player.pacshow(Player.x1,Player.y1);
+		Pinky.show(Pinky.x1,Pinky.y1);
+		Inky.show(Inky.x1,Inky.y1);
+		Blinky.show(Blinky.x1,Blinky.y1);
+		Clyde.show(Clyde.x1,Clyde.y1);
+		Pinky.ghostpacmanCollision();
+		Blinky.ghostpacmanCollision();
+		Inky.ghostpacmanCollision();
+		Clyde.ghostpacmanCollision();
+		//Player.pacshow(298,420);
 		//SDL_RenderCopy(rend,Pinky.ghost,&Pinky.png[Pinky.direction],&Pinky.ghostpos);
 		SDL_RenderPresent( rend );
 	}
